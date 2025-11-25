6d8a3acb9468ade31c01e216a690ec89322e1b61
Shahar187 <shahar.shitrit187@gmail.com>
2025-11-25 20:55:16 +0200

    After editing kelly's files,and files names and change also refering to other files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function loginAsWalker() {
+  window.location.href = "login.html?role=walker";
+}
+
+function loginAsVolunteer() {
+  window.location.href = "login.html?role=volunteer";
+}
+
+function goToRegister() {
+  window.location.href = "register.html";
+}
