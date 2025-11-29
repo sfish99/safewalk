4bb5a16b6617fdea135e181def343f6da9ba0aa0
Shahar187 <shahar.shitrit187@gmail.com>
2025-11-29 19:22:21 +0200

    fix refernce

--- includes/login_walker_form.php
+++ includes/login_walker_form.php
@@ -24,6 +24,6 @@ if (!password_verify($password, $hashedPassword)) {
 $_SESSION['walker_id'] = $id;
 $_SESSION['walker_name'] = $name;
 
-header("Location: Home_walker.php.php");
+header("Location: Home_walker.php");
 exit;
 ?>
