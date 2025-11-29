af866174fd813b6893b03b187002707fe77b3eb0
Shahar187 <shahar.shitrit187@gmail.com>
2025-11-29 17:32:08 +0200

    Fixed JS function

--- js/index.js
+++ js/index.js
@@ -6,17 +6,12 @@ function loginAsVolunteer() {
   window.location.href = "login.html?role=volunteer";
 }
 
-<<<<<<< HEAD
 function goToRegister(type) {
   if (type === "walker"){
     window.location.href = "includes/register_walker.html";
   }
-  else {
+  if (type === "volunteer") {
     window.location.href = "includes/register_volunteer.html";
 
   }
-=======
-function goToRegister() {
-  window.location.href = "includes/register_volunteer.html";
->>>>>>> 71acbb7a74c890faa78ca4c88e3e1760f53b43bb
 }
