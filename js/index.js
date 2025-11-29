5776d885adab4b911e6bb03db7b9891ca6a2fe9c
kellydeitch <kelly.deitch@gmail.com>
2025-11-29 17:14:49 +0200

    Saving my changes

--- js/index.js
+++ js/index.js
@@ -6,6 +6,12 @@ function loginAsVolunteer() {
   window.location.href = "login.html?role=volunteer";
 }
 
-function goToRegister() {
-  window.location.href = "register.html";
+function goToRegister(type) {
+  if (type === "walker"){
+    window.location.href = "includes/register_walker.html";
+  }
+  else {
+    window.location.href = "includes/register_volunteer.html";
+
+  }
 }
