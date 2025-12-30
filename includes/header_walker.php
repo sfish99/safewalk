3f3c5fa05faf8ca30622335feb36174eec86b317
sfish99 <sharonfispam@gmail.com>
2025-12-30 21:08:18 +0200

    added return button

--- includes/header_walker.php
+++ includes/header_walker.php
@@ -1,8 +1,8 @@
 <header class="header-dashboard">
     <a href="logout_walker.php" class="logout-btn">התנתקות</a>
+    <a href="home_walker.php" class="back-btn">התנתקות</a>
 <div class="dashboard-content-wrapper">
         <img src="../images/logo.png" alt="SafeWalk Logo" class="logo">
-
          <h2 class="welcome-text">שלום <?php echo htmlspecialchars($walkerName); ?> 🌟</h2>
 </div>
 </header>
