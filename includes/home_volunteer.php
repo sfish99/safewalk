3be806742dc39930cc049ebbc1e7dc8a525bcb52
Shahar187 <shahar.shitrit187@gmail.com>
2025-12-31 17:27:37 +0200

    home css united to one

--- includes/home_volunteer.php
+++ includes/home_volunteer.php
@@ -17,7 +17,7 @@ $volName = $_SESSION['volunteer_name'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SafeWalk - דף ראשי מתנדבת</title>
-    <link rel="stylesheet" href="../css/home_volunteer.css">
+    <link rel="stylesheet" href="../css/home.css">
 
 </head>
 <body>
